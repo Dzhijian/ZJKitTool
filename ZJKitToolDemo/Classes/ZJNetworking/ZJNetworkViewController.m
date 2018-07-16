3a9ad08694baff39b986b1721e552a90c6f73298
dengzj <dengzj@canplay.com.cn>
2018-07-16 16:00:09 +0800

    优化网络与 pickerView

--- ZJKitToolDemo/Classes/ZJNetworking/ZJNetworkViewController.m
+++ ZJKitToolDemo/Classes/ZJNetworking/ZJNetworkViewController.m
@@ -5,6 +5,13 @@
 //  Created by 邓志坚 on 2018/7/6.
 //  Copyright © 2018年 kapokcloud. All rights reserved.
 //
+/**
+ *  ZJKitTool
+ *
+ *  GitHub地址：https://github.com/Dzhijian/ZJKitTool
+ *
+ *  本库会不断更新工具类，以及添加一些模块案例，请各位大神们多多指教，支持一下,给个Star。😆
+ */
 
 #import "ZJNetworkViewController.h"
 
--- ZJKitToolDemo/Classes/ZJNetworking/ZJNetworking.m
+++ ZJKitToolDemo/Classes/ZJNetworking/ZJNetworking.m
@@ -5,6 +5,13 @@
 //  Created by 邓志坚 on 2018/7/5.
 //  Copyright © 2018年 kapokcloud. All rights reserved.
 //
+/**
+ *  ZJKitTool
+ *
+ *  GitHub地址：https://github.com/Dzhijian/ZJKitTool
+ *
+ *  本库会不断更新工具类，以及添加一些模块案例，请各位大神们多多指教，支持一下,给个Star。😆
+ */
 
 #import "ZJNetworking.h"
 #import "AFNetworkActivityIndicatorManager.h"
