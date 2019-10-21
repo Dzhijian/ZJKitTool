77209840f02f4ec6208385bcf09ffe6c69a54d4a
james <646724452@qq.com>
2019-10-21 18:36:28 +0800

    修改按钮

--- ZJKitTool/ZJPickerView/ZJPickBaseView.m
+++ ZJKitTool/ZJPickerView/ZJPickBaseView.m
@@ -108,12 +108,13 @@
 
 -(UIButton *)leftBtn{
     if (!_leftBtn) {
-        _leftBtn = [[UIButton alloc]initWithFrame:CGRectMake(6, 8, 60, 28)];
+        _leftBtn = [[UIButton alloc]initWithFrame:CGRectMake(6, 8, 90, 28)];
         _leftBtn.backgroundColor = [UIColor lightTextColor];
         _leftBtn.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin;
-        _leftBtn.titleLabel.font = kFontSize(15);
+        _leftBtn.titleLabel.font = kFontSize(14);
         [_leftBtn setTitleColor:kDefaultThemeColor forState:UIControlStateNormal];
         [_leftBtn setTitle:@"取消" forState:UIControlStateNormal];
+        _leftBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
         [_leftBtn addTarget:self action:@selector(leftBtnClickAction:) forControlEvents:UIControlEventTouchUpInside];
     }
     return _leftBtn;
@@ -122,12 +123,13 @@
 #pragma mark - 右边确定按钮
 - (UIButton *)rightBtn {
     if (!_rightBtn) {
-        _rightBtn = [[UIButton alloc]initWithFrame:CGRectMake(self.alertView.frame.size.width - 65, 8, 60, 28)];
+        _rightBtn = [[UIButton alloc]initWithFrame:CGRectMake(self.alertView.frame.size.width - 65, 8, 90, 28)];
         _rightBtn.backgroundColor = kBRToolBarColor;
         _rightBtn.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin;
-        _rightBtn.titleLabel.font = kFontSize(15);
+        _rightBtn.titleLabel.font = kFontSize(14);
         [_rightBtn setTitleColor:kDefaultThemeColor forState:UIControlStateNormal];
         [_rightBtn setTitle:@"确定" forState:UIControlStateNormal];
+        _leftBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
         [_rightBtn addTarget:self action:@selector(rightBtnClickAction:) forControlEvents:UIControlEventTouchUpInside];
     }
     return _rightBtn;
